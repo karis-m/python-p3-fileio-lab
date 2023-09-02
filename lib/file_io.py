ea417a5635e3b3a91e1b8d40ba4a2ad65840bdcb
karis-m <moses.kamau@student.moringaschool.com>
2023-09-01 22:56:39 -0400

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    with open(file_name, mode='w', encoding='utf-8') as log_file:log_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(file_name, mode='a', encoding='utf-8') as log_file:log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    text_file = open(file_name, encoding='utf-8')
+    print(f"\n{text_file.read()}")
+
+write_file("lib/log_file.txt","Log 1: 5 bananas added")
+append_file("lib/log_file.txt","Log 2: 3 bananas subtracted")
+read_file("lib/log_file.txt")
